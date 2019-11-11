83537946006adf3fa33bd36bdc1eafb2031915dc
Jendrasi <>
2019-11-11 04:47:40 -0500

    Simple Server
    Beginnings of backend, hosting frontend

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const foo = 'hello';
-console.log(foo);
+// Define public constant variables relevant to backend
+const ssr = 'ssr.xenial.com/?env=';
+
+
+const express = require('express');
+
+const app = express();
+app.listen(3000, () => console.log('Listening at 3000'));
+app.use(express.static('frontend'));
+
