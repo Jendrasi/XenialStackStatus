55eae37894099b700181256d80204081419f5cb5
Jendrasi <>
2019-11-11 04:44:02 -0500

    Frontend Placeholder
    Add a frontend placeholder, will be hosted by node server

---
+++ frontend/index.js
@@ -0,0 +1,4 @@
+// Define public constants relevant to frontent
+const superComponentHtml = '<div id="" class="superComp"></div>';
+const subComponentHtml = '<div id="" class="subComp"></div>';
+const stackHtml = '<div id="" class="stack"></div>';
